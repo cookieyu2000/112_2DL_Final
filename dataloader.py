1f46ca83d16ab8e7300cbc57c8e6e7e447bdcc1f
Ping-Yen,Yu <cookie2000891109@gmail.com>
2024-06-01 23:19:38 +0800

    完整

--- dataloader.py
+++ dataloader.py
@@ -11,12 +11,12 @@ import torch
 import json
 import pickle
 
-image_paths ='data/augmented_images'
+# 定義圖像路徑和輸出資料夾
+image_paths = 'data/augmented_images'
 output_images_folder = 'images'
-cache_path = 'data/cache.pkl'  # 定义缓存文件路径
+cache_path = 'data/cache.pkl'  # 定義快取文件路徑
 
-
-# 定义支持缓存的自定义数据集类
+# 定義支援快取的自定義數據集類
 class CachedDataset(Dataset):
     def __init__(self, dataframe, label_map, transform=None, cache_file=None):
         self.dataframe = dataframe
@@ -25,6 +25,7 @@ class CachedDataset(Dataset):
         self.cache = {}
         self.cache_file = cache_file
 
+        # 如果快取文件存在，載入快取
         if cache_file and os.path.exists(cache_file):
             self.load_cache()
 
@@ -32,9 +33,11 @@ class CachedDataset(Dataset):
         return len(self.dataframe)
 
     def __getitem__(self, idx):
-        if idx in self.cache:  # 检查缓存中是否已有该图像
+        # 檢查快取中是否已有該圖像
+        if idx in self.cache:
             image, label = self.cache[idx]
         else:
+            # 載入圖像並應用變換
             img_path = self.dataframe.iloc[idx]['image_path']
             image = Image.open(img_path).convert('RGB')
             
@@ -42,24 +45,25 @@ class CachedDataset(Dataset):
                 image = self.transform(image)
             
             label = self.dataframe.iloc[idx]['label']
-            # 将疾病对应到数字
-            label = self.label_map[label]
-            self.cache[idx] = (image, label)  # 将图像和标签存储到缓存中
+            label = self.label_map[label]  # 將疾病對應到數字
+            self.cache[idx] = (image, label)  # 將圖像和標籤存儲到快取中
 
         return image, label
 
+    # 保存快取
     def save_cache(self):
         with open(self.cache_file, 'wb') as f:
             pickle.dump(self.cache, f)
         print(f'Cache saved to {self.cache_file}')
 
+    # 載入快取
     def load_cache(self):
         with open(self.cache_file, 'rb') as f:
             self.cache = pickle.load(f)
         print(f'Cache loaded from {self.cache_file}')
 
 
-# 定义函数绘制并保存饼图
+# 定義函數繪製並保存餅圖
 def plot_pie_chart(data, title, filename):
     label_counts = data['label'].value_counts()
     fig, ax = plt.subplots()
@@ -67,60 +71,59 @@ def plot_pie_chart(data, title, filename):
     ax.axis('equal')
     ax.set_title(title)
     
-    # 保存图像
+    # 保存圖像
     save_path = os.path.join(output_images_folder, filename)
     plt.savefig(save_path)
     plt.close(fig)
     
     # print(f'{title} detailed counts:\n{label_counts}\n')
 
+# 讀取數據
 train_df = pd.read_csv('data/augmented_labels.csv')
 # print(f'total number of images: {len(train_df)}')
 
 standard = pd.read_json('data/label_num_to_disease_map.json', typ='series')
-standard_map = dict(standard)
+standard_map = dict(standard) # 將疾病名稱映射到數值標籤
 # print(f'Number of classes: {len(standard)}')
 
-# 将疾病名称映射到数值标签
+# 將疾病名稱映射到數值標籤
 reversed_standard_map = {v: int(k) for k, v in standard_map.items()}
 train_df['label'] = train_df['label'].map(standard_map)
-train_df['image'] = train_df['image_path'].apply(lambda x: x.split('/')[-1]) # Extract the image name from the path
+train_df['image'] = train_df['image_path'].apply(lambda x: x.split('/')[-1]) # 提取圖像名稱
 # print(f'total number of images: {len(train_df["image"])}')
 
+# 劃分數據集
 train_data, valid_data = train_test_split(train_df, test_size=0.2, stratify=train_df['label'], random_state=42, shuffle=True)
-
 valid_data, test_data = train_test_split(valid_data, test_size=0.5, stratify=valid_data['label'], random_state=42, shuffle=True)
 
-
-# # 绘制并保存饼图
+# # 繪製並保存餅圖
 # plot_pie_chart(train_data, 'Training Data Distribution', 'train_data_distribution.png')
 # plot_pie_chart(valid_data, 'Validation Data Distribution', 'valid_data_distribution.png')
 # plot_pie_chart(test_data, 'Test Data Distribution', 'test_data_distribution.png')
 
-# 計算權重
+# 計算類別權重
 class_counts = train_data['label'].value_counts()
 total_samples = len(train_data)
 class_weights = {label: total_samples / count for label, count in class_counts.items()}
 
-# # 逐行打印类别权重
+# # 逐行打印類別權重
 # print("Class Weights:")
 # for label, weight in class_weights.items():
 #     print(f'Class {label}: {weight}')
 
-# 定义图像变换
+# 定義圖像變換
 transform = transforms.Compose([
-    transforms.Resize((224, 224)),  # 根据需要调整尺寸
+    transforms.Resize((224, 224)),  # 調整尺寸
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
-
-# 创建数据集实例
+# 創建數據集實例
 train_dataset = CachedDataset(train_data, reversed_standard_map, transform=transform, cache_file=cache_path)
 valid_dataset = CachedDataset(valid_data, reversed_standard_map, transform=transform, cache_file=cache_path)
 test_dataset = CachedDataset(test_data, reversed_standard_map, transform=transform, cache_file=cache_path)
 
-# 保存缓存
+# 保存快取
 train_dataset.save_cache()
 valid_dataset.save_cache()
 test_dataset.save_cache()
@@ -129,7 +132,7 @@ test_dataset.save_cache()
 # print(f'Validation dataset size: {len(valid_dataset)}')
 # print(f'Test dataset size: {len(test_dataset)}')
 
-# DataLoader
+# 創建DataLoader
 train_loader = DataLoader(train_dataset, batch_size=Config.BATCH_SIZE, shuffle=True)
 valid_loader = DataLoader(valid_dataset, batch_size=Config.BATCH_SIZE, shuffle=False)
 test_loader = DataLoader(test_dataset, batch_size=Config.BATCH_SIZE, shuffle=False)
